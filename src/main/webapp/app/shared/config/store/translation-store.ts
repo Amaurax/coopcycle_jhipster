2445fa5a7d440361a9bbcc6d3a43f5422958bf32
goguilla <amaury.goguillot@etu.univ-grenoble-alpes.fr>
2023-04-08 13:52:53 +0200

    init

---
+++ src/main/webapp/app/shared/config/store/translation-store.ts
@@ -0,0 +1,26 @@
+import { Module } from 'vuex';
+
+export const translationStore: Module<any, any> = {
+  state: {
+    currentLanguage: localStorage.getItem('currentLanguage') || 'fr',
+    languages: {
+      en: { name: 'English' },
+      fr: { name: 'Français' },
+      de: { name: 'Deutsch' },
+      ja: { name: '日本語' },
+      ru: { name: 'Русский' },
+      es: { name: 'Español' },
+      // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+    },
+  },
+  getters: {
+    currentLanguage: state => state.currentLanguage,
+    languages: state => state.languages,
+  },
+  mutations: {
+    currentLanguage(state, newLanguage) {
+      state.currentLanguage = newLanguage;
+      localStorage.setItem('currentLanguage', newLanguage);
+    },
+  },
+};
